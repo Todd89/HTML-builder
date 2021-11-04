b6c101dc7fb83f9a4ad3d22bf7a309da00f69bb2
Todd89 <alex-89fish@tut.by>
2021-11-04 14:44:00 +0300

    feat: solve task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,57 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+
+  let pathOfSource = path.join(__dirname) + "/files";
+  let pathOfTarget = path.join(__dirname) + "/files-copy";
+
+  fs.stat(`${pathOfTarget}`, function (err) {
+    if(!err) {
+      fs.readdir(pathOfTarget, (err, files) => {
+        if(err) {
+          return console.error(err);
+        } else {
+          for(let file of files) {
+            fs.unlink(`${pathOfTarget}/${file}`, function (err) {
+              if(err) {
+                console.log(err);
+              }
+            });
+          }
+          fs.readdir(pathOfSource, { withFileTypes: true }, (err, data) => {
+            data.map(el => {
+              if(el.isFile() == true) {
+                fs.copyFile(`${pathOfSource}/${el.name}`, `${pathOfTarget}/${el.name}`, (err) => {
+                  if(err) {
+                    console.log("Error Found:", err);
+                  }
+                })
+              }
+            })
+          })
+        }
+
+      })
+    } else {
+      fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if(err) {
+          return console.error(err);
+        }
+        fs.readdir(pathOfSource, { withFileTypes: true }, (err, data) => {
+          data.map(el => {
+            if(el.isFile() == true) {
+              fs.copyFile(`${pathOfSource}/${el.name}`, `${pathOfTarget}/${el.name}`, (err) => {
+                if(err) {
+                  console.log("Error Found:", err);
+                }
+              })
+            }
+          })
+        })
+      });
+    }
+  });
+}
+
+copyDir()
